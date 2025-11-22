94592816999b9d2625c1be0992d3c6cc313713b4
Sankalp <160457369+TheKnightProtocol@users.noreply.github.com>
2025-11-22 21:08:32 +0530

    Create Binary Search.java
    Given a sorted array arr[] and an integer k, find the position(0-based indexing) at which k is present in the array using binary search. If k doesn't exist in arr[] return -1. 
    
    Note: If multiple occurrences are there, please return the smallest index.
    
    Examples:
    
    Input: arr[] = [1, 2, 3, 4, 5], k = 4
    Output: 3
    Explanation: 4 appears at index 3.
    Input: arr[] = [11, 22, 33, 44, 55], k = 445
    Output: -1
    Explanation: 445 is not present.
    Input: arr[] = [1, 1, 1, 1, 2], k = 1
    Output: 0
    Explanation: 1 appears at index 0.
    Constraints:
    1 ≤ arr.size() ≤ 105
    1 ≤ arr[i] ≤ 106
    1 ≤ k ≤ 106

---
+++ Binary Search.java
@@ -0,0 +1,17 @@
+class Solution {
+    int binarysearch(int[] arr, int k) {
+        int left = 0, right = arr.length - 1, ans = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == k) {
+                ans = mid;
+                right = mid - 1;
+            } else if (arr[mid] < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+}

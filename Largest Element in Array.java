2cab4bc0bd71eb2c242ff535a5907cb5a0b6a7e4
Sankalp <160457369+TheKnightProtocol@users.noreply.github.com>
2025-12-10 21:27:40 +0530

    Create Largest Element in Array.java
    Given an array arr[]. The task is to find the largest element and return it.
    
    Examples:
    
    Input: arr[] = [1, 8, 7, 56, 90]
    Output: 90
    Explanation: The largest element of the given array is 90.
    Input: arr[] = [5, 5, 5, 5]
    Output: 5
    Explanation: The largest element of the given array is 5.
    Input: arr[] = [10]
    Output: 10
    Explanation: There is only one element which is the largest.
    Constraints:
    1 <= arr.size()<= 106
    0 <= arr[i] <= 106

---
+++ Largest Element in Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int largest(int arr[]) {
+        if (arr == null || arr.length == 0) return 0;
+        
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}

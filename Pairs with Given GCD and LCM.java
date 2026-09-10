d6c0b5ad4c777c424bfb4a9a4cb31747abefdcb8
Sankalp <160457369+TheKnightProtocol@users.noreply.github.com>
2026-09-10 15:23:22 +0530

    Create Pairs with Given GCD and LCM.java
    Given two integers x and y representing the GCD and LCM of two unknown positive integers a and b, count the number of valid pairs (a, b) satisfying these conditions. Note that (a, b) and (b, a) are counted as distinct pairs when a ≠ b.
    
    Examples:
    
    Input: x = 2, y = 12
    Output: 4
    Explanation: The valid pairs are (2, 12), (4, 6), (6, 4), and (12, 2), since each pair has GCD = 2 and LCM = 12.
    Input: x = 6, y = 4
    Output: 0
    Explanation: LCM must always be a multiple of GCD. Since y is not divisible by x, no valid pair exists.
    Constraints:
    1 ≤ x, y ≤ 104

---
+++ Pairs with Given GCD and LCM.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pairCount(int x, int y) {
+        // code here
+        if ( y % x != 0) return 0;
+        int n = y/x;
+        int k = 0;
+        for (int p =2; p * p <=n; p++){
+            if( n % p ==0){
+                k++;
+                while(n % p ==0) n /= p;
+            }
+        }
+        if (n > 1) k++;
+        return 1 << k;
+    }
+}
